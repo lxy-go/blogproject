98c11a53cb32539abab28ed302bd0af86fb79c03
lixiyan <554458482@qq.com>
2019-05-14 17:16:06 +0800

    增加信息注释

--- src/main/java/com/lxy/blogproject/BlogprojectApplication.java
+++ src/main/java/com/lxy/blogproject/BlogprojectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//启注解事务管理
+//启注解事务管理1
 @EnableTransactionManagement  // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @MapperScan(basePackages = "com.lxy.blogproject.dao", markerInterface = MyMapper.class)
 public class BlogprojectApplication {
